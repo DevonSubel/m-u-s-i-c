0fc63191ef97f4b470b4dbb3661aea0c39c5d3e9
Declan Kelly <dkelly.home@gmail.com>
2018-10-22 10:32:07 -0700

    Add sqlite jdbc, and some basic db interaction code

--- src/main/java/com/github/se307/App.java
+++ src/main/java/com/github/se307/App.java
@@ -1,13 +1,39 @@
 package com.github.se307;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+	public static void main(String[] args) {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);
+
+			statement.executeUpdate("drop table if exists person");
+			statement.executeUpdate("create table person (id integer, name string)");
+			statement.executeUpdate("insert into person values(1, 'leo')");
+			statement.executeUpdate("insert into person values(2, 'yui')");
+
+			ResultSet rs = statement.executeQuery("select * from person");
+
+			while (rs.next()) {
+				System.out.println("name = " + rs.getString("name"));
+				System.out.println("id = " + rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				System.err.println(e);
+			}
+		}
+	}
 }
