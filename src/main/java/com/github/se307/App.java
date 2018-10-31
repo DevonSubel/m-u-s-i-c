f37adde4417f2e4a3ac42823a66dc4ea69e49c5f
Declan Kelly <dkelly.home@gmail.com>
2018-10-30 18:22:54 -0700

    Add database driver and local resources singleton

--- src/main/java/com/github/se307/App.java
+++ src/main/java/com/github/se307/App.java
@@ -1,39 +1,19 @@
 package com.github.se307;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class App {
 	public static void main(String[] args) {
-		Connection connection = null;
+		Connection connection = DatabaseDriver.getConnection();
+
 		try {
-			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-			Statement statement = connection.createStatement();
-			statement.setQueryTimeout(30);
+			Statement stmnt = connection.createStatement();
 
-			statement.executeUpdate("drop table if exists person");
-			statement.executeUpdate("create table person (id integer, name string)");
-			statement.executeUpdate("insert into person values(1, 'leo')");
-			statement.executeUpdate("insert into person values(2, 'yui')");
-
-			ResultSet rs = statement.executeQuery("select * from person");
-
-			while (rs.next()) {
-				System.out.println("name = " + rs.getString("name"));
-				System.out.println("id = " + rs.getInt("id"));
-			}
+			stmnt.executeUpdate("INSERT INTO song(name, artist_name, album_name) VALUES ('dye', 'tycho', 'awake')");
 		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-		} finally {
-			try {
-				if (connection != null)
-					connection.close();
-			} catch (SQLException e) {
-				System.err.println(e);
-			}
+			e.printStackTrace();
 		}
 	}
 }
