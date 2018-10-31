359f7091aa357f9ec4be1b3998390e532604f80f
Amanda <mndcheng@github.com>
2018-10-30 20:36:29 -0700

    WIP

--- src/main/java/com/github/se307/App.java
+++ src/main/java/com/github/se307/App.java
@@ -1,13 +1,71 @@
 package com.github.se307;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+    private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new Browser(),900,600, Color.web("#666970"));
+        stage.setScene(scene);
+        
+        URL url = this.getClass().getResource("/com/github/se307/App.css");
+        scene.getStylesheets().add(url.toString());        
+        stage.show();
+    }
+ 
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+class Browser extends Region {
+ 
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+     
+    public Browser() {
+        //apply the styles
+        getStyleClass().add("browser");
+        
+        // load the web page
+        URL url = this.getClass().getResource("/com/github/se307/index.html");
+        webEngine.load(url.toString());
+
+        //add the web view to the scene
+        getChildren().add(browser);
+ 
+    }
+    private Node createSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
+ 
+    @Override protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
+    }
+ 
+    @Override protected double computePrefWidth(double height) {
+        return 750;
+    }
+ 
+    @Override protected double computePrefHeight(double width) {
+        return 500;
     }
 }
