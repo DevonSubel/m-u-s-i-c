6c86f1f436f24b3033abb47eeefd1eba68b1b01f
Declan Kelly <dkelly.home@gmail.com>
2018-11-02 11:38:17 -0700

    [#49] Add database interface
    * Ignore sqlite database files
    
    * Add sqlite jdbc, and some basic db interaction code
    
    * Ignore classpath file
    
    * Ignore DS_Store
    
    * Add database driver and local resources singleton

--- src/main/java/com/github/se307/App.java
+++ src/main/java/com/github/se307/App.java
@@ -1,13 +1,19 @@
 package com.github.se307;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+	public static void main(String[] args) {
+		Connection connection = DatabaseDriver.getConnection();
+
+		try {
+			Statement stmnt = connection.createStatement();
+
+			stmnt.executeUpdate("INSERT INTO song(name, artist_name, album_name) VALUES ('dye', 'tycho', 'awake')");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
