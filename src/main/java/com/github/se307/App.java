27cff55eebc7aa66ff1a675d234e352f98ee69f7
mndcheng <31120215+mndcheng@users.noreply.github.com>
2018-11-06 18:29:52 -0800

    WIP (#50)
    adds javafx to maven dependency and basic html css files to project

--- src/main/java/com/github/se307/App.java
+++ src/main/java/com/github/se307/App.java
@@ -4,7 +4,27 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class App {
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	private Scene scene;
+	
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new Browser(),2560,1600, Color.web("#211E1E"));
+        stage.setScene(scene);
+        
+//        URL css = this.getClass().getResource("/com/github/se307/App.css");
+//        scene.getStylesheets().add(css.toString());
+        stage.show();
+    }
+	
 	public static void main(String[] args) {
 		Connection connection = DatabaseDriver.getConnection();
 
@@ -12,6 +32,8 @@ public class App {
 			Statement stmnt = connection.createStatement();
 
 			stmnt.executeUpdate("INSERT INTO song(name, artist_name, album_name) VALUES ('dye', 'tycho', 'awake')");
+			
+			launch(args);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
