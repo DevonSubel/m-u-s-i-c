47f8ecca02bf74ed54422ba6f849052b54fc4e75
Maxence Weyrich <maxwey@users.noreply.github.com>
2018-11-27 19:31:09 -0800

    Add song to db (#64)
    * Added a SongBuilder class to build song objects. Added query to get all songs in databse. Reformat.
    
    * Song builder improvements and fixes to the resource leaks
    
    * fixed merge issues

--- src/main/java/com/github/se307/SongDatabaseDriver.java
+++ src/main/java/com/github/se307/SongDatabaseDriver.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -18,73 +20,134 @@ import org.apache.logging.log4j.LogManager;
 public class SongDatabaseDriver {
 
 	private static SongDatabaseDriver singleton;
-	
+
+	public static final String NAME_F = "name";
+	public static final String ARTIST_NAME_F = "artist_name";
+	public static final String ALBUM_NAME_F = "album_name";
+	public static final String SONG_LENGTH_F = "song_length";
+	public static final String GENRE_ID_F = "genre_id";
+	public static final String SONG_YEAR_F = "song_year";
+	public static final String BPM_F = "bpm";
+	public static final String ADDITIONAL_NOTES_F = "additional_notes";
+	public static final String URI_F = "uri";
 
 	private final String UPDATE_STATEMENT_CONST = "UPDATE song SET ? = ? WHERE id = ?;";
 	private final String DELETE_STATEMENT_CONST = "DELETE FROM song WHERE id = ?";
 	private final String QUERY_FIELDS_CONST = "SELECT * FROM song WHERE id = ?";
+	private final String CREATE_STATEMENT_CONST = "INSERT INTO song(%s) VALUES(%s)";
+	private final String QUERY_ALL_SONG_KEYS_CONST = "SELECT key FROM song";
 
-	// private static final String CREATE_STATEMENT_CONST = "INSERT INTO song("
-	
- 	private Connection dbConnection;
-	
+	private Connection dbConnection;
+
 	private PreparedStatement updateStatement;
 	private PreparedStatement deleteStatement;
 	private PreparedStatement queryStatement;
-	
-	
- 	private static final Logger logger = LogManager.getLogger();
- 	
- 	/**
- 	 * Initialize the Singleton by creating all the prepared statements that are used inside the SongSet classes
- 	 */
+	private PreparedStatement queryAllStatement;
+
+	private static final Logger logger = LogManager.getLogger();
+
+	/**
+	 * Initialize the Singleton by creating all the prepared statements that are
+	 * used inside the SongSet classes
+	 */
 	private SongDatabaseDriver() {
 		try {
 			dbConnection = DatabaseDriver.getConnection();
-			
+
 			updateStatement = dbConnection.prepareStatement(UPDATE_STATEMENT_CONST);
 			deleteStatement = dbConnection.prepareStatement(DELETE_STATEMENT_CONST);
 			queryStatement = dbConnection.prepareStatement(QUERY_FIELDS_CONST);
-					
-			
-		} catch(SQLException e) {
+			queryAllStatement = dbConnection.prepareStatement(QUERY_ALL_SONG_KEYS_CONST);
+
+		} catch (SQLException e) {
 			logger.error("Failed to load prepared statements for SongDatabaseDriver: " + e.getMessage());
 		}
 	}
-	
+
 	/**
-	 * Update the song with the provided column with the specified value. 
-	 * This method is synchronized (thread-safe).
+	 * Create a new song using the specified field names and their associated
+	 * values. The order of the field name and the values must be in the same order
+	 * and of the same size This method is thread-safe (unsynchronized).
 	 * 
-	 * @param colName 	the name of the column being updated
-	 * @param value		the value to update the database with
-	 * @param key		the key identifying the Song entry to update
-	 * @return			true if the update was successful
+	 * @param fieldList the list of field (column) names to insert
+	 * @param paramList the list of the values to be inserted
+	 * @return the primary key of the newly created song 0 will be returned if there
+	 *         was an error adding the song to the database
+	 */
+	public long createSong(String[] fieldList, Object[] paramList) {
+		long songPrimaryKey = 0;
+
+		if (fieldList.length != paramList.length || fieldList.length <= 0) {
+			logger.error("Number of fields is empty or not matching with number of values");
+			return songPrimaryKey;
+		}
+
+		// Insert the correct number of ? characters
+		String qMarkString = "";
+		for (int i = 0; i < fieldList.length - 1; i++) {
+			qMarkString = qMarkString + "?, ";
+		}
+		qMarkString = qMarkString + "?";
+		String createSongStatement = String.format(CREATE_STATEMENT_CONST, qMarkString, qMarkString);
+
+		try {
+			// Create the prepared statement
+			PreparedStatement creationStatement = dbConnection.prepareStatement(createSongStatement,
+					Statement.RETURN_GENERATED_KEYS);
+
+			// Insert the values into the prepared statement
+			for (int i = 1; i <= fieldList.length; i++) {
+				creationStatement.setString(i, fieldList[i - 1]);
+				creationStatement.setObject(i + fieldList.length, paramList[i - 1]);
+			}
+
+			// execute the statement and save the primary key
+			creationStatement.executeUpdate();
+			ResultSet keys = creationStatement.getGeneratedKeys();
+			if (keys.next()) {
+				songPrimaryKey = keys.getLong(1);
+			}
+		} catch (SQLException e) {
+			logger.error("Failed to execute createSong prepared statement: " + e.getMessage());
+			return songPrimaryKey;
+		}
+		return songPrimaryKey;
+	}
+
+	/**
+	 * Update the song with the provided column with the specified value. This
+	 * method is synchronized (thread-safe).
+	 * 
+	 * @param colName the name of the column being updated
+	 * @param value   the value to update the database with
+	 * @param key     the key identifying the Song entry to update
+	 * @return true if the update was successful
 	 */
 	public synchronized boolean updateSong(String colName, Object value, long key) {
 		try {
 			this.updateStatement.setString(1, colName);
 			this.updateStatement.setObject(2, value);
 			this.updateStatement.setLong(3, key);
-			
+
 			this.updateStatement.executeUpdate();
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			logger.error("Failed to execute updateSong prepared statement: " + e.getMessage());
 			return false;
 		}
 		return true;
 	}
-	
+
 	/**
-	 * Remove the song identified by the key from the database.
-	 * This method is synchronized (thread-safe).
-	 * @param songKey		the key identifier of the song being added
-	 * @return				true if the update was successful
+	 * Remove the song identified by the key from the database. This method is
+	 * synchronized (thread-safe).
+	 * 
+	 * @param songKey the key identifier of the song being added
+	 * @return true if the update was successful
 	 */
 	public synchronized boolean removeSong(long songKey) {
 		try {
 			this.deleteStatement.setLong(1, songKey);
-			
+
 			this.deleteStatement.executeUpdate();
 		} catch(SQLException e) {
 			logger.error("Failed to execute removeSong prepared statement: " + e.getMessage());
@@ -92,37 +155,96 @@ public class SongDatabaseDriver {
 		}
 		return true;
 	}
-	
-	/**
-	 * Returns a result set for the song identified by the songKey
-	 * The result set contains all the fields for an entry in the song table
-	 * @param songKey	the key of the song being retrieved
-	 * @return			the ResultSet of the query. null is returned on error.
-	 */
-	public synchronized ResultSet getSong(long songKey) {
-		ResultSet returnValue = null;
 
+	/**
+	 * Returns a result set for the song identified by the songKey The result set
+	 * contains all the fields for an entry in the song table
+	 * 
+	 * @param songKey the key of the song being retrieved
+	 * @return a SongBuilder with the values pre-populated with the results of the query. 
+	 *			null is returned on error.
+	 */
+	public synchronized Song.SongBuilder getSong(long songKey) {
+		Song.SongBuilder sb = new Song.SongBuilder();
 		try {
 			this.queryStatement.setLong(1, songKey);
-			returnValue = this.queryStatement.executeQuery();
-		} catch(SQLException e) {
-			logger.error("Failed to execute getSong prepared statement: " + e.getMessage());
-		}
+			ResultSet rs = this.queryStatement.executeQuery();
+			
+			sb.songName = rs.getString(NAME_F);
+			sb.artistName = rs.getString(ARTIST_NAME_F);
+			sb.albumName = rs.getString(ALBUM_NAME_F);
 
-		return returnValue;
+			sb.songLength = rs.getInt(SONG_LENGTH_F);
+			if (rs.wasNull())
+				sb.songLength = null;
+
+			sb.genreID = rs.getInt(GENRE_ID_F);
+			if (rs.wasNull())
+				sb.genreID = null;
+
+			sb.songYear = rs.getInt(SONG_YEAR_F);
+			if (rs.wasNull())
+				sb.songYear = null;
+
+			sb.bpm = rs.getInt(BPM_F);
+			if (rs.wasNull())
+				sb.bpm = null;
+
+			sb.additionalNotes = rs.getString(ADDITIONAL_NOTES_F);
+			sb.songURL = rs.getString(URI_F);
+			
+			rs.close();
+		} catch (SQLException e) {
+			logger.error("Failed to execute getSong prepared statement: " + e.getMessage());
+			return null;
+		}
+		return sb;
 	}
-	
+
 	/**
-	 * Get the singleton instance. 
-	 * A singleton is created if it does not already exist.
-	 * @return 		a reference to the singleton
+	 * Returns a list of the keys of every song in the database. This method is
+	 * thread-safe.
+	 * 
+	 * @return ArrayList containing all the keys.
+	 */
+	public ArrayList<Long> getAllSongs() {
+		ArrayList<Long> songKeys = new ArrayList<Long>();
+
+		try {
+			ResultSet returnValue = this.queryAllStatement.executeQuery();
+			while (returnValue.next()) {
+				songKeys.add(returnValue.getLong(0));
+			}
+
+			returnValue.close();
+		} catch (SQLException e) {
+			logger.error("Failed to execute getSong prepared statement: " + e.getMessage());
+			return null;
+		}
+		return songKeys;
+	}
+
+	/**
+	 * Get the singleton instance. A singleton is created if it does not already
+	 * exist.
+	 * 
+	 * @return a reference to the singleton
 	 */
 	public static synchronized SongDatabaseDriver getInstance() {
-		if(SongDatabaseDriver.singleton == null) {
+		if (SongDatabaseDriver.singleton == null) {
 			SongDatabaseDriver.singleton = new SongDatabaseDriver();
 		}
 
 		return SongDatabaseDriver.singleton;
 	}
+
+	/**
+	 * Return an String array with the fields used in the database.
+	 * 
+	 * @return array of field names
+	 */
+	public static String[] getFields() {
+		return new String[] { NAME_F, ARTIST_NAME_F, ALBUM_NAME_F, SONG_LENGTH_F, GENRE_ID_F, SONG_YEAR_F, BPM_F,
+				ADDITIONAL_NOTES_F, URI_F };
+	}
 }
- 
--- src/main/java/com/github/se307/SongSet.java
+++ src/main/java/com/github/se307/SongSet.java
@@ -10,20 +10,18 @@ import org.apache.logging.log4j.LogManager;
  *
  */
 public abstract class SongSet {
-	
+
 	protected static final SongSetDatabaseDriver DB_DRIVER = SongSetDatabaseDriver.getInstance();
 	protected static final Logger logger = LogManager.getLogger();
 	
 	/*
-	 * SongSet pk: 
-	 * > 0 is a PK to a set in the database
-	 * 0 is the default PK assigned when a set is not stored to the db
-	 * < 0 is the PK assigned to a set that has been deleted (or should no longer be available)
+	 * SongSet pk: > 0 is a PK to a set in the database 0 is the default PK assigned
+	 * when a set is not stored to the db < 0 is the PK assigned to a set that has
+	 * been deleted (or should no longer be available)
 	 */
 	protected long songSetKey;
 	protected String songSetName;
-	
-	
+
 	/**
 	 * Create a new (empty) SongSet with no connection to the database
 	 */
@@ -31,38 +29,39 @@ public abstract class SongSet {
 		songSetKey = 0;
 		songSetName = "Untitled";
 	}
-	
+
 	/**
 	 * Create a SongSet using the database values
-	 * @param songSet		the key of the entry into the database table for SongSets
+	 * 
+	 * @param songSet the key of the entry into the database table for SongSets
 	 */
 	public SongSet(long databasePrimaryKey) {
 		songSetKey = databasePrimaryKey;
-		songSetName = (String)SongSet.DB_DRIVER.querySongSet("name", databasePrimaryKey);
+		songSetName = (String) SongSet.DB_DRIVER.querySongSet("name", databasePrimaryKey);
 	}
-	
+
 	/**
-	 * Get all the songs from the Song Set. The Song objects are not inflated by default.
+	 * Get all the songs from the Song Set. The Song objects are not inflated by
+	 * default.
 	 * 
-	 * @return 		list containing all the songs. List is not modifiable.
+	 * @return list containing all the songs. List is not modifiable.
 	 */
 	public abstract List<Song> getSongs();
-	
+
 	/**
-	 * Saves the Song Set to the database
-	 * A name is automatically chosen if not given.
-	 * TODO: consider requiring user to input the song set title
+	 * Saves the Song Set to the database A name is automatically chosen if not
+	 * given. TODO: consider requiring user to input the song set title
 	 */
 	public abstract void save();
-	
-	
+
 	/**
-	 * Set the SongSet name 
-	 * @param songSetName	the new name of the Set
-	 * @return 				true if the song was not in the set and was successfully added
+	 * Set the SongSet name
+	 * 
+	 * @param songSetName the new name of the Set
+	 * @return true if the song was not in the set and was successfully added
 	 */
 	public boolean setSongSetName(String songSetName) {
-		if(songSetKey == 0 || SongSet.DB_DRIVER.updateSongSet("name", songSetName, songSetKey)) {
+		if (songSetKey == 0 || SongSet.DB_DRIVER.updateSongSet("name", songSetName, songSetKey)) {
 			this.songSetName = songSetName;
 			return true;
 		} else {
@@ -71,13 +70,14 @@ public abstract class SongSet {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Delete the SongSet from the database
-	 * @return		true on success
+	 * 
+	 * @return true on success
 	 */
 	public boolean deleteSongSet() {
-		if(songSetKey == 0 || SongSet.DB_DRIVER.removeSongSet(songSetKey)) {
+		if (songSetKey == 0 || SongSet.DB_DRIVER.removeSongSet(songSetKey)) {
 			// Mark this song as having been deleted
 			songSetKey = -1;
 			// TODO: handle when the change was successful
@@ -88,9 +88,10 @@ public abstract class SongSet {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Get the name of the SongSet
+	 * 
 	 * @return
 	 */
 	public String getSongSetName() {
@@ -99,17 +100,17 @@ public abstract class SongSet {
 
 	/**
 	 * Get whether this SongSet has been deleted
+	 * 
 	 * @return
 	 */
 	public boolean isDeleted() {
 		return (songSetKey < 0);
 	}
-	
-	
+
 	/*
 	 * Not implemented, but ideas on what song sets could contain
 	 */
 	// public void reverse();
 	// public void sortBy();
-		
+
 }
