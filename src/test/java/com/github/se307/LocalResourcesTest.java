66ba9231910f1a7b94f274efd55bf8f8b41f686c
Declan Kelly <dkelly.home@gmail.com>
2018-12-02 13:51:08 -0800

    Add local resources tests

---
+++ src/test/java/com/github/se307/LocalResourcesTest.java
@@ -0,0 +1,26 @@
+package com.github.se307;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class LocalResourcesTest {
+
+	@Test
+	public void checkUserHome() {
+		LocalResources localResources = LocalResources.getResources();
+
+		assertEquals(System.getProperty("user.home"), localResources.getUserHome().toString());
+	}
+
+	@Test
+	public void checkLocalResourceFolder() {
+		LocalResources localResources = LocalResources.getResources();
+
+		String expectedPath = Paths.get(System.getProperty("user.home"), ".m-u-s-i-c").toString();
+		assertEquals(expectedPath, localResources.getUserResourcesDir().toString());
+	}
+
+}
--- src/test/java/com/github/se307/UserSettingsManagerTest.java
+++ src/test/java/com/github/se307/UserSettingsManagerTest.java
@@ -1,9 +1,9 @@
 package com.github.se307;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.nio.file.Paths;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,63 +16,62 @@ import javafx.scene.paint.Color;
 /**
  * Unit test for simple App.
  */
-public class UserSettingsManagerTest 
-{
-	
+public class UserSettingsManagerTest {
+
 	private void cleanSettings() {
-		File settingsFile = Paths.get(System.getProperty("user.home"), ".m-u-s-i-c", "settings.xml").toFile();
-		
+		File settingsFile = LocalResources.getResources().getUserResourcesDir().resolve("settings.xml").toFile();
+
 		if (settingsFile.exists()) {
 			assertTrue(settingsFile.delete());
 		}
 	}
-	
+
 	@Before
 	public void beforeTest() {
 		cleanSettings();
 	}
-	
+
 	@After
 	public void afterTest() {
 		cleanSettings();
-		
+
 		UserSettingsManager.resetUserSettings();
 	}
-	
+
 	@Test
 	public void getDefaultSettings() {
 		UserSettings settings = UserSettingsManager.getUserSettings();
-		
+
 		assertEquals("black", settings.getGuiColor());
 	}
-	
+
 	@Test
 	public void modifySettings() {
 		UserSettings settings = UserSettingsManager.getUserSettings();
 
 		assertEquals("black", settings.getGuiColor());
-		
+
 		settings.setGuiColor("red");
-		
+
 		UserSettingsManager.saveUserSettings();
-		
+
 		settings = UserSettingsManager.getUserSettings();
-		
+
 		assertEquals(settings.getGuiColor(), "red");
 	}
-	
+
 	@Test
 	public void modifyGuiColor() {
 		UserSettings settings = UserSettingsManager.getUserSettings();
-		
+
 		assertEquals(Color.BLACK, settings.getFXColor());
-		
+
 		settings.setGuiColor(Color.RED);
-		
+
 		UserSettingsManager.saveUserSettings();
-		
+
 		settings = UserSettingsManager.getUserSettings();
-		
+
 		assertEquals(Color.RED, settings.getFXColor());
 	}
 }
